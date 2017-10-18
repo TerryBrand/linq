f9d0a6e240b267c07726f9b623f857f1c7c4a699
unknown <terrybrand98@gail.com>
2017-10-18 11:53:43 -0400

    Headfirst c# linq

---
+++ Linq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = new int[] { 0, 12, 44, 36, 92, 54, 13, 8};
+
+            var result = from v in values
+                         where v < 37
+                         orderby v
+                         select v;
+            foreach (int i in result)
+                Console.WriteLine("{0}", i);
+            Console.ReadKey();
+        }
+    }
+}
